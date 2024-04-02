9a2ba534568434ce0d1d1d433a448336c5df324e
jack <jackg2667@protonmail.com>
2024-04-02 15:56:07 +0100

    adding earthquake warning sound

--- src/scripts/overlay.js
+++ src/scripts/overlay.js
@@ -4,36 +4,43 @@ References:
   Youtube. Available at: https://www.youtube.com/watch?v=MBaw_6cPmAw [Accessed 8 Feb. 2024].
 */
 
+// Get reference to the audio element
+const sirenSound = document.getElementById("sirenSound");
+
 // variables for opening, closing buttons and overlay
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
-// when the open button for the modal is clicked, find any corresponding elements
-// with the id of #modal and open them
+// When the open button for the modal is clicked, start playing the sound
 openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
+    // Start playing the sound
+    sirenSound.play();
   });
 });
 
-// when a modal is open and the close button is clicked close whatever modal is currently opened/active
+// When the close button for the modal is clicked, stop playing the sound
 closeModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = button.closest(".modal");
     closeModal(modal);
+    // Stop playing the sound
+    sirenSound.pause();
+    sirenSound.currentTime = 0; // Reset the sound to the beginning
   });
 });
 
-// opens the modal and sets an active class to reveal it, if it can't find a modal return without opening the modal
+// Function to open the modal
 function openModal(modal) {
   if (modal == null) return;
   modal.classList.add("active");
   overlay.classList.add("active");
 }
 
-// closes the modal and remove an active class from the modal elements to hide it 
+// Function to close the modal
 function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove("active");
